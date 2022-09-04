458ea2c03467029d1d7c0fde930103aa129f76be
Fran Jurinec <franjurinec@live.com>
2022-09-04 15:21:37 +0000

    Exercise 2.19 & 2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Filter from './Filter'
+import Message from './Message'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
 import personService from './services/persons'
@@ -11,6 +12,9 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  const [successMessage, setSuccessMessage] = useState()
+  const [errorMessage, setErrorMessage] = useState()
 
   const fetchPersons = () => {
     // Get persons from DB
@@ -48,7 +52,15 @@ const App = () => {
       if(window.confirm(`${existingPerson.name} is already added to phonebook, replace the old number with a new one?`))
         personService
           .update(existingPerson.id, newPerson)
-          .then(fetchPersons)
+          .then(response => {
+            setSuccessMessage(`Updated ${response.data.name}'s number`)
+            setTimeout(() => {setSuccessMessage(null)}, 5000)
+          })
+          .catch(error => {
+            setErrorMessage(`Information of ${newPerson.name} has already been removed from the server`)
+            setTimeout(() => {setErrorMessage(null)}, 5000)
+          })
+          .finally(fetchPersons)
       setNewName('')
       setNewNumber('')
       return
@@ -60,6 +72,8 @@ const App = () => {
         setPersons(persons.concat(response.data))
         setNewName('')
         setNewNumber('')
+        setSuccessMessage(`Added ${newPerson.name}`)
+        setTimeout(() => {setSuccessMessage(null)}, 5000)
       })
   }
 
@@ -67,7 +81,15 @@ const App = () => {
     if (window.confirm(`Delete ${person.name}?`))
       personService
         .deleteById(person.id)
-        .then(fetchPersons)
+        .then(_ => {
+          setSuccessMessage(`Deleted ${person.name}`)
+          setTimeout(() => {setSuccessMessage(null)}, 5000)
+        })
+        .catch(error => {
+          setErrorMessage(`Information of ${person.name} has already been removed from the server`)
+          setTimeout(() => {setErrorMessage(null)}, 5000)
+        })
+        .finally(fetchPersons)
   }
 
   const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
@@ -75,6 +97,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Message message={successMessage} type='success' />
+      <Message message={errorMessage} type='error' />
       <Filter value={filter} onFilterChange={handleFilterChange} />
       <PersonForm onSubmit={addPerson} 
         newName={newName} 
