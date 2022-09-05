28264303512cdcca423dc1e2fbeca03f29ba1d04
Fran Jurinec <franjurinec@live.com>
2022-09-05 18:50:17 +0000

    Exercise 3.7

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 const port = process.env.PORT ?? 3001
 
 app.use(express.json())
+
+app.use(morgan('tiny'))
 
 let persons = [
     { 
