ac9c09a72f84e52f04370c0892677030b1769741
Fran Jurinec <franjurinec@live.com>
2022-10-27 20:34:53 +0000

    Exercises 8.8-8.10

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,35 @@
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from '../queries'
+
+const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS)
+
+  if (!props.show) return null
+  return (
+    <div>
+      <h2>authors</h2>
+      {result.loading ? (
+        <div>Loading...</div>
+      ) : (
+        <table>
+          <tbody>
+            <tr>
+              <th></th>
+              <th>born</th>
+              <th>books</th>
+            </tr>
+            {result.data.allAuthors.map((a) => (
+              <tr key={a.name}>
+                <td>{a.name}</td>
+                <td>{a.born}</td>
+                <td>{a.bookCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  )
+}
+
+export default Authors
