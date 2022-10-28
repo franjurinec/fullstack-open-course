44da6ef9d8f03d5649598ee31497a0d6f85fe69a
Fran Jurinec <franjurinec@live.com>
2022-10-28 22:56:07 +0000

    Exercise 8.17

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -14,7 +14,9 @@ export const ALL_BOOKS = gql`
   query {
     allBooks {
       title
-      author
+      author {
+        name
+      }
       published
     }
   }
@@ -46,6 +48,14 @@ export const EDIT_AUTHOR = gql`
     editAuthor(name: $name, setBornTo: $setBornTo) {
       name
       born
+    }
+  }
+`
+
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      value
     }
   }
 `
