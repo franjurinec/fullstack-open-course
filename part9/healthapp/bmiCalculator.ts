0fdc7f4fac6e95013d9ede201b3770b8e48a605f
= <=>
2022-11-24 21:56:08 +0000

    Exercise 9.1

---
+++ part9/healthapp/bmiCalculator.ts
@@ -0,0 +1,23 @@
+type BmiState = "Underweight (Severe thinness)" 
+| "Underweight (Moderate thinness)" 
+| "Underweight (Mild thinness)" 
+| "Normal (healthy weight)" 
+| "Overweight (Pre-obese)" 
+| "Obese (Class I)" 
+| "Obese (Class II)" 
+| "Obese (Class III)" 
+
+const calculateBmi = (height: number, weight: number): BmiState => {
+    const bmi = weight / (height / 100) ** 2 
+
+    if (bmi < 16) return "Underweight (Severe thinness)"
+    if (bmi < 17) return "Underweight (Moderate thinness)"
+    if (bmi < 18.5) return "Underweight (Mild thinness)"
+    if (bmi < 25) return "Normal (healthy weight)"
+    if (bmi < 30) return "Overweight (Pre-obese)"
+    if (bmi < 35) return "Obese (Class I)"
+    if (bmi < 40) return "Obese (Class II)"
+    return "Obese (Class III)"
+}
+
+console.log(calculateBmi(180, 74))
