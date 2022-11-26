06516c8a12e76316c3a77689d2075992d9053803
= <=>
2022-11-26 22:28:02 +0000

    Exercise 9.5 Nit

--- part9/healthapp/index.ts
+++ part9/healthapp/index.ts
@@ -11,13 +11,11 @@ app.get('/bmi', (req, res) => {
   const height = Number(req.query.height)
   const weight = Number(req.query.weight)
 
-  if (!height || !weight) {
-    res.json({error: 'malformatted parameters'})
-    return
-  }
+  if (!height || !weight)
+    return res.json({error: 'malformatted parameters'})
     
 
-  res.json({
+  return res.json({
     weight,
     height,
     bmi: calculateBmi(height, weight)
