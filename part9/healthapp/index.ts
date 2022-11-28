fb9bb2c9fd90891116fc6e6c70690a4ef1580a8f
= <=>
2022-11-28 22:35:00 +0000

    Exercise 9.7

--- part9/healthapp/index.ts
+++ part9/healthapp/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercise } from './exerciseCalculator';
 
 const app = express();
+
+const PORT = process.env.PORT ?? 3003;
+
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -14,7 +19,6 @@ app.get('/bmi', (req, res) => {
   if (!height || !weight)
     return res.json({ error: 'malformatted parameters' });
 
-
   return res.json({
     weight,
     height,
@@ -22,7 +26,23 @@ app.get('/bmi', (req, res) => {
   });
 });
 
-const PORT = 3003;
+app.post('/exercises', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  if (!req.body.daily_exercises || !req.body.target)
+    return res.json({error: 'parameters missing'});
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+  const hours: number[] = req.body.daily_exercises;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+  const target: number = req.body.target;
+
+  if (!hours || !target)
+    return res.json({error: 'malformed parameters'});
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const result = calculateExercise(hours, target);
+  return res.json(result);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
