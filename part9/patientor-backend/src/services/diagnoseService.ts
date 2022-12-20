ceb00bc0cc8025e715fa0dd8e898855d1246b3d9
Fran Jurinec <franjurinec@live.com>
2022-12-20 19:58:03 +0100

    Refactoring to src

--- part9/patientor-backend/src/services/diagnoseService.ts
+++ part9/patientor-backend/src/services/diagnoseService.ts
@@ -1,4 +1,4 @@
-import diagnoseData from '../data/diagnoses.json';
+import diagnoseData from '../../data/diagnoses.json';
 import { Diagnose } from '../types';
 
 const diagnoses: Diagnose[] = diagnoseData;
